8701cdabf4bc67f7e799f8b21f71f2754d7e880e
Sheamus Burns <sheamusburns@gmail.com>
2018-01-06 21:29:26 -0500

    Login component styling, validation message text

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -18,24 +18,27 @@ class Login extends Component {
     });
   }
 
-  submitLogin () {
-    let that = this
+  submitLogin (event) {
+    let that = this;
+    let element = event.target;
+    element.disabled = true;
     if (this.state.username && this.state.password) {
+      // add logic to login service
       console.log(this.state.username);
       console.log(this.state.password);
       this.setState({username: '', password: '', message: 'success', messageClass: 'success'});
       // console.log(this.getState('password'))
     } else {
       console.log('username or password are blank');
-      this.setState({message: 'failure', messageClass: 'failure'});
+      this.setState({message: 'please fill out all fields', messageClass: 'failure'});
     }
-    window.setTimeout(function () {
+    window.setTimeout(function(event) {
       that.setState({message: ''});
+      element.disabled = false;
     }, 2000)
   }
 
   handleChange (event) {
-    console.log(event)
     if (event.target) {
       if (event.target.name === 'username') {
         this.setState({username: event.target.value});
@@ -47,19 +50,19 @@ class Login extends Component {
   }
 
   componentDidUpdate () {
-    console.log(this.state);
+    // console.log(this.state);
   }
 
   render () {
     return (
       <div className='login-form'>
-        <h1>Login</h1>
+        <h2>Login</h2>
         <label>Username: </label>
         <input name='username' onChange={this.handleChange} value={this.state.username} />
         <label>Password: </label>
-        <input name='password' onChange={this.handleChange} value={this.state.password} />
+        <input name='password' type='password' onChange={this.handleChange} value={this.state.password} />
         <button name='submit' onClick={this.submitLogin}>login</button>
-        <div className={this.state.messageClass}>
+        <div id='notify-message' className={this.state.messageClass}>
           {this.state.message}
         </div>
       </div>
@@ -67,4 +70,4 @@ class Login extends Component {
   }
 }
 
-export default Login
+export default Login;
