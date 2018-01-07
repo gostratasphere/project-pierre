f6fd42aa86346343e7ca394dfbbda1177d0b8319
Sheamus Burns <sheamusburns@gmail.com>
2018-01-07 00:07:17 -0500

    basic eventList functionality

--- frontend/src/components/EventList.js
+++ frontend/src/components/EventList.js
@@ -6,12 +6,38 @@ class EventList extends Component {
     this.state = {};
   }
 
+  generateRows(){
+  	let data = [['my Event', 'Jan 2'], ['Party', 'Feb 20']]
+  	let rows = [];
+  	data.map((i)=> {
+  		rows.push(<EventRow eventName={i[0]} eventDate={i[1]}></EventRow>);
+  	})
+  	return rows;
+  }
+
 	render() {
 		return (
-			<div>hello</div>
+			<div>
+				{this.generateRows()}
+			</div>
 		)
 	}
 	
+}
+
+class EventRow extends Component {
+	constructor(props){
+		super(props);
+		this.state = {}
+	}
+	componentWillMount(){
+		console.log(this.props)
+	}
+	render(){
+		return (
+			<div>{this.props.eventName} {this.props.eventDate}</div>
+			)
+	}
 }
 
 export default EventList;
