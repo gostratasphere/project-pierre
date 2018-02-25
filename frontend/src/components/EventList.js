8a88cadc51bf8147b99242f58f03b088a1c268a6
Sheamus Burns <sheamusburns@gmail.com>
2018-02-25 14:13:24 -0500

    disable placeholder date and functions to manage it

--- frontend/src/components/EventList.js
+++ frontend/src/components/EventList.js
@@ -30,23 +30,17 @@ class EventList extends Component {
   }
 
   generateRows(){
-  	let data = [['Unicorn Herders Conference', 'A gathering of the brightest minds in mythological husbandry', 'Jan 21'],
-  							['Quantum Wormhole Ice Cream Social', 'Gathering of Friends of the Society for Advanced Physical Systems. No Ultimate Frisbee this time.', 'Feb 20'],
-  							['Ubiquitous Presence Roundtable', 'An unconference for teleheads, pundits, and social misfits', 'Oct 22'],
-  							['Omnidirectional Antennae Dialectic', 'Twice yearly multi-threaded conversation on the nature of being in a universe that listens.', 'March 4']]
   	let rows = [];
-
-  	// eslint-disable-next-line
-  	data.map((i)=> {
-  		rows.push(<EventRow eventName={i[0]} eventDescription={i[1]} eventDate={i[2]}></EventRow>);
-  	})
+  	// let data = [['Unicorn Herders Conference', 'A gathering of the brightest minds in mythological husbandry', 'Jan 21'],
+  	// 						['Quantum Wormhole Ice Cream Social', 'Gathering of Friends of the Society for Advanced Physical Systems. No Ultimate Frisbee this time.', 'Feb 20'],
+  	// 						['Ubiquitous Presence Roundtable', 'An unconference for teleheads, pundits, and social misfits', 'Oct 22'],
+  	// 						['Omnidirectional Antennae Dialectic', 'Twice yearly multi-threaded conversation on the nature of being in a universe that listens.', 'March 4']]
+  	// data.map((i)=> {
+  	// 	rows.push(<EventRow eventName={i[0]} eventDescription={i[1]} eventDate={i[2]}></EventRow>);
+  	// })
   	this.state.events.map(i => {
   		rows.push(<EventRow eventName={i[0]} eventDescription={i[1]} eventDate={i[2]}></EventRow>)
   	})
-  	
-
-  	// 	console.log(JSON.stringify(d.body));
-  	
   	return rows;
   	
   }
